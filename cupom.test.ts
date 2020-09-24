01bb001b8d3a555434c7287736798c700c0168e0
whendev <whendel.morais.9@gmail.com>
2020-09-24 16:21:30 -0300

    exercise solved

--- cupom.test.ts
+++ cupom.test.ts
@@ -72,22 +72,28 @@ test('Inscrição estadual vazia', () => {
 
 test('Exercício 2 - customizado', () => {
 
-  // Defina seus próprios valores para as variáveis a seguir
-  cupom.dados.nome_loja = "";
-  cupom.dados.logradouro = "";
-  cupom.dados.numero = 0;
+//   // Defina seus próprios valores para as variáveis a seguir
+  cupom.dados.nome_loja = "LOJAS AMERICANAS S.A.";
+  cupom.dados.logradouro = "R SACADURA CABRAL";
+  cupom.dados.numero = 102;
   cupom.dados.complemento = "";
-  cupom.dados.bairro = "";
-  cupom.dados.municipio = "";
-  cupom.dados.estado = "";
-  cupom.dados.cep = "";
-  cupom.dados.telefone = "";
-  cupom.dados.observacao = "";
-  cupom.dados.cnpj = "";
-  cupom.dados.inscricao_estadual = "";
+  cupom.dados.bairro = "GAMBOA";
+  cupom.dados.municipio = "RIO DE JANEIRO";
+  cupom.dados.estado = "RJ";
+  cupom.dados.cep = "20.221-160";
+  cupom.dados.telefone = "(21) 2206-6708";
+  cupom.dados.observacao = "47.11-3-02 Comercio varejista de mercadorias em geral";
+  cupom.dados.cnpj = "33.014.556/0001-96";
+  cupom.dados.inscricao_estadual = "85.687.08-5";
 
   //E atualize o texto esperado abaixo
   expect(cupom.dados_loja()).toBe(
-    `
+    `LOJAS AMERICANAS S.A.
+R SACADURA CABRAL, 102 
+GAMBOA - RIO DE JANEIRO - RJ
+CEP:20.221-160 Tel (21) 2206-6708
+47.11-3-02 Comercio varejista de mercadorias em geral
+CNPJ: 33.014.556/0001-96
+IE: 85.687.08-5
 `);
 });
